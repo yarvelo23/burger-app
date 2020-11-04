23117a5ebab4b98dd6ba6b5541f20ca6f9d2494b
yerson <yerson.a06@gmail.com>
2020-11-03 20:40:33 -0500

    connection and object creation

--- config/orm.js
+++ config/orm.js
@@ -1,78 +1,31 @@
+// require connection js
+const connection = require("../config/connection.js");
 
-// Here is the O.R.M. where you write functions that takes inputs and conditions
-// and turns them into database commands like SQL.
 
-var connection = require("./connection.js");
-
-function printQuestionMarks(num) {
-  var arr = [];
-
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-
-  return arr.toString();
-}
-
-function objToSql(ob) {
-  // column1=value, column2=value2,...
-  var arr = [];
-
-  for (var key in ob) {
-    arr.push(key + "=" + ob[key]);
-  }
-
-  return arr.toString();
-}
-
-var orm = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+// creating object
+const orm = {
+  selectAll: function (tableName, cb) {
+    const queryString = " SELECT * FROM " + tableName + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
       cb(result);
+    })
+  },
+
+  insertOne: function (tableName, column, value, cb) {
+    const queryString = `INSERT INTO ${tableName} (${column}, devoured) VALUES ("${value}", 0);`
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
+      cb(result)
     });
   },
-  // vals is an array of values that we want to save to cols
-  // cols are the columns we want to insert the values into
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
 
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
-
-    console.log(queryString);
-
-    connection.query(queryString, vals, function(err, result) {
-      if (err) {
-        throw err;
-      }
+  updateOne: function (tableName, column, condition, value, cb) {
+    const queryString = `UPDATE ${tableName} SET ${column}=${condition} WHERE id=${value};`
+    connection.query(queryString, function (err, result) {
+      if (err) throw err;
       cb(result);
-    });
-  },
-  // objColVals would be the columns and values that you want to update
-  // an example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
-
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += condition;
-
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
+    })
   }
 };
 

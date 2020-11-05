a1f35297346e708bb88c709d6b7c5949dd49418a
yerson <yerson.a06@gmail.com>
2020-11-04 22:07:14 -0500

    adding jawsdb condition

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,10 @@
+// setup mysql connection
 var mysql = require("mysql");
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -7,8 +12,9 @@ var connection = mysql.createConnection({
   password: "play2306$",
   database: "burgers_db"
 });
+}
 
-
+// make connection
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,4 +23,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+// export connection for ORM
 module.exports = connection;

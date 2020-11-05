a160a4dabe33a3bb43b25a08b6f31c51c24ecd2d
yerson <yerson.a06@gmail.com>
2020-11-04 22:24:38 -0500

    update syntax

--- db/schema.sql
+++ db/schema.sql
@@ -8,6 +8,6 @@ DROP TABLE IF EXISTS burgers;
 CREATE TABLE burgers (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
-    devoured BOOL DEFAULT false,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );

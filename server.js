e09365bd05a3df7afb7f9798617f7084e380346f
yerson <yerson.a06@gmail.com>
2020-11-04 22:52:16 -0500

    syntax update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var express = require("express");
+const express = require("express");
 
-var PORT = process.env.PORT || 8000;
-var app = express();
+const PORT = process.env.PORT || 8080;
+const app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -10,11 +10,14 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// express-handlebars
 var exphbs = require("express-handlebars");
 
+// require express-handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// import server routes
 var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
